bd25899397c2723fae5e337e877c6d1f0130ba02
Burence1 <burensomondi@gmail.com>
2021-02-28 00:58:29 +0300

    implementing form input functionality

--- js/script.js
+++ js/script.js
@@ -1,48 +1,82 @@
  $(document).ready(function() {
-        $("#des").click(function() {
-          $("#design").show();
-          $("#des").hide();
-        })});  
-          $("#develop").click(function(){
-          $("#dev").show();
-          $("#develop").hide();
-          });
-        $("#product").click(function(){
-          $("#manage").show();
-          $("#product").hide();
-        });
+  $("#des").click(function() {
+    $("#design").show();
+    $("#des").hide();
+  })});  
+    $("#develop").click(function(){
+    $("#dev").show();
+    $("#develop").hide();
+    });
+  $("#product").click(function(){
+    $("#manage").show();
+    $("#product").hide();
+  });
 
 $(document).ready(function () {
-        $("#design").click(function () {
-          $("#design").hide();
-          $("#des").show();
-        })});
-        $("#dev").click(function () {
-          $("#dev").hide();
-          $("#develop").show();
-        });
-        $("#manage").click(function () {
-          $("#manage").hide();
-          $("#product").show();
-        });
+  $("#design").click(function () {
+    $("#design").hide();
+    $("#des").show();
+  })});
+  $("#dev").click(function () {
+    $("#dev").hide();
+    $("#develop").show();
+  });
+  $("#manage").click(function () {
+    $("#manage").hide();
+    $("#product").show();
+  });
 
-// $("#pic3").on(
-//         {
-//           mouseenter: function () {
-            
-//             $('#pic3').on('mouseenter', function () {
-//               $('#mighty',this);
-//             });
-//           }
-//           mouseleave: function () {
-            
-//             $('#pic3').on('mouseleave', function () {
-//               $('#mighty').hide();
-//             });
-//           }
-//         });
+$('#pic1').hover(function () {
+  $(".gallery1",this).fadeToggle(1000);
+});
+$('#pic2').hover(function () {
+  $(".gallery2", this).fadeToggle(1000);
+});
+$('#pic3').hover(function () {
+  $(".gallery3", this).fadeToggle(1000);
+});
+$('#pic4').hover(function () {
+  $(".gallery4", this).fadeToggle(1000);
+});
+$('#pic5').hover(function () {
+  $(".gallery5", this).fadeToggle(1000);
+});
+$('#pic6').hover(function () {
+  $(".gallery6", this).fadeToggle(1000);
+});
+$('#pic7').hover(function () {
+  $(".gallery7", this).fadeToggle(1000);
+});
+$('#pic8').hover(function () {
+  $(".gallery8", this).fadeToggle(1000);
+});
 
-function text() {
-  var text = document.getElementById("name").value;
-  alert( `${text} we have received your message. Thank you for reaching out to us`);
-}
+//  function text() {
+//   var text = document.getElementById("name").value;
+//   alert( `${text} we have received your message. Thank you for reaching out to us`);
+// }
+
+$(document).ready(function () {
+  //event.preventDefault();
+  $("#submit").click(function () {
+    var x = $("form").serializeArray();
+    console.log(x);
+    $.each(x, function (i, field) {
+        console.log(x);
+        $("#out").push(`${field.name}:${field.value} `);
+        console.log(x);
+        return false;
+      });
+  });
+});
+
+// function getFormObj(data) {
+//   var formObj = {};
+//   console.log(formObj)
+//   var inputs = $(`#submit${data}`).serializeArray();
+//   $.each(inputs, function (i, input) {
+//     console.log(formObj)
+//     formObj[input.name] = input.value;
+//   });
+//   return formObj;
+// }
